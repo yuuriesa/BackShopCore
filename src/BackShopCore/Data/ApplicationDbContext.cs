57902b8e682476e13c8c88f53b3b4fdabc5a68a2
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-09-04 14:37:59 -0300

    feat: enable dependency injection for DbContext with DbContextOptions

--- src/BackShopCore/Data/ApplicationDbContext.cs
+++ src/BackShopCore/Data/ApplicationDbContext.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BackShopCore.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
