11d20ddd7433413093a882e4e8eb27abbebd2afc
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-09-04 14:48:35 -0300

    feat: Implemented Connection String

--- src/BackShopCore/Data/ApplicationDbContext.cs
+++ src/BackShopCore/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace BackShopCore.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connetionString = "";
+            var connetionString = "Server=localhost;Database=BackShopCore;User=SA;Password=Password123!;TrustServerCertificate=True";
             optionsBuilder.UseSqlServer(connectionString: connetionString);
         }
     }
