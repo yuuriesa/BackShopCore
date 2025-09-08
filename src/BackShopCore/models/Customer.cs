1f63ec6edbe96f2bbf991f4ff275b0348bbff9a2
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-09-08 14:42:22 -0300

    feat: add public properties (Customer)

---
+++ src/BackShopCore/models/Customer.cs
@@ -0,0 +1,19 @@
+namespace BackShopCore.Models
+{
+    public class Customer
+    {
+        //private properties
+
+        //public properties
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+        //constructors
+
+        //public methods
+
+        //private methods
+    }
+}
